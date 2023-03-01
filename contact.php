69a67eddd3884c60ebe50bb0a57c822148499a70
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-03-01 16:05:03 -0700

    Updated Design/ Content

---
+++ contact.php
@@ -0,0 +1,46 @@
+<?php
+
+#Receive user input
+$email = $_POST['email_address'];
+$name = $_POST['name']
+$message = $_POST['message'];
+
+#Filter user input
+function filter_email_header($form_field) {  
+return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
+}
+
+$email_address  = filter_email_header($email);
+
+#Send email
+$headers = "From: $email_address";
+$sent = mail('maggie.chew.89+WEBCONTACT@gmail.com', 'Feedback Form Submission', $message, $headers);
+
+#Thank user or notify them of a problem
+if ($sent) {
+
+?><html>
+<head>
+<title>Thank You</title>
+</head>
+<body>
+<h1>Thank You</h1>
+<p>Thank you for your feedback.</p>
+</body>
+</html>
+<?php
+
+} else {
+
+?><html>
+<head>
+<title>Something went wrong</title>
+</head>
+<body>
+<h1>Something went wrong</h1>
+<p>We could not send your feedback. Please try again.</p>
+</body>
+</html>
+<?php
+}
+?>
